3a6b99efe4e0154b362bd7b3fa6f01bd72598122
Ben Dorman <ben.dorman@changesoftware.co.uk>
2024-11-17 17:26:02 +0000

    Updated Account Schema & added Seed Data

--- Ensek.Smart/Ensek.Smart.Data/Migrations/20241117171703_IntialCreate.Designer.cs
+++ Ensek.Smart/Ensek.Smart.Data/Migrations/20241117171703_IntialCreate.Designer.cs
@@ -3,6 +3,7 @@ using Ensek.Smart.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Ensek.Smart.Data.Migrations
 {
     [DbContext(typeof(SmartDatabaseContext))]
-    partial class SmartDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20241117171703_IntialCreate")]
+    partial class IntialCreate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- Ensek.Smart/Ensek.Smart.Data/Model/Account.cs
+++ Ensek.Smart/Ensek.Smart.Data/Model/Account.cs
@@ -1,15 +1,8 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace Ensek.Smart.Data.Model
+﻿namespace Ensek.Smart.Data.Model
 {
-
     public class Account
     {
-        [Key]
-        public Guid Id { get; set; }
-
-        [Timestamp]
-        public required byte[] Version { get; set; }
+        public int AccountId { get; set; }
 
         public required string FirstName { get; set; }
 
